7f9f9f2dd8368875e863cf459d40c9500ba46149
Vous <siddhaertha@gmail.com>
2013-03-24 19:11:53 -0400

    Updated files

---
+++ build/lib/gcore/generator/scores.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from gcore import *
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
-config = {
-    'description': 'GCore Scoreboard Generator',
-    'author': 'Lord Bonaparte',
-    'url': 'someurl',
-    'download_url': 'someurl',
-    'author_email': 'eesitmaarja@gmail.com',
-    'version': '0.1',
-    'install_requires': [''],
-    'packages': ['gcore'],
-    'scripts': [],
-    'name': 'gcore'
-}
-
-setup(**config)
+setup(
+    name='GCore',
+    version='0.0.1',
+    author='Bonaparte',
+    author_email='eestimaarja@gmail.com',
+    packages=['gcore', 'gcore.generator', 'gcore.parser'],
+    scripts=[],
+    url='',
+    license='',
+    description='Scoreboard Generator',
+    long_description=open('README.md').read()
+)
