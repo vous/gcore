250dc4767a757879833d5291c34193f0ad6ef76c
Vous <siddhaertha@gmail.com>
2013-03-24 18:21:09 -0400

    Fixed an error in Voter.py

--- gcore/parser/Voter.py
+++ gcore/parser/Voter.py
@@ -6,10 +6,10 @@ Created on Sun Mar 24 18:17:38 2013
 """
 
 class Voter:
-    def __init__(self, name="", image="", points={}, _id=0):
+    def __init__(self, name="", image="", points=dict(), _id=0):
         self.name = name
         self.image = image
-        self.points = dict()
+        self.points = points
         self._id = _id
         
     def __str__(self):
